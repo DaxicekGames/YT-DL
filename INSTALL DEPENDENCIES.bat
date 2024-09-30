568c827b7f0e676d34546655fe57f10d8716d9e2
DaxicekGames <114874974+DaxicekGames@users.noreply.github.com>
2024-09-30 21:51:46 +0200

    Additions to make running the program simpler

---
+++ INSTALL DEPENDENCIES.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Installing FFmpeg...
+pip install ffmpeg
+echo Installing FFmpeg-python...
+pip install ffmpeg-python
+echo Installing yt_dlp...
+pip install yt_dlp
+echo ================================================
+echo Dependencies installed! You can now run RUN.bat
+echo ================================================
+pause
